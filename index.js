29e8c4d071c1745e9168cabdf223b48163be6988
Shahin992 <shahinkhan5979@gmail.com>
2024-02-17 13:39:13 +0600

    auth changed

--- index.js
+++ index.js
@@ -183,7 +183,7 @@ app.get("/users", auth, async (req, res) => {
     .toArray();
   res.status(200).send(result);
 });
-app.get("/users/:email", auth, async (req, res) => {
+app.get("/users/:email", async (req, res) => {
   const email = req.params.email;
   const result = await userCollection.findOne(
     { email },
